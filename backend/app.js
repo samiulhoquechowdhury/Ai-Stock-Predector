3af1a76ad6ccb7d89cb5bf3b003f447931f9a56c
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-04-01 21:28:43 +0530

    stock rotes used in app

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 
 const authRoutes = require("./routes/authRoutes");
+const stockRoutes = require("./routes/stockRoutes");
 
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/stocks", stockRoutes);
 
 mongoose
   .connect(process.env.MONGO_URI, {
