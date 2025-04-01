3c7b8f505e458da2250612d339125448fdc7fbcb
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-04-01 22:04:15 +0530

    updated model for user

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,12 +1,10 @@
 const mongoose = require("mongoose");
 
-const UserSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { timestamps: true }
-);
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  portfolio: [{ type: String }],
+});
 
 module.exports = mongoose.model("User", UserSchema);
