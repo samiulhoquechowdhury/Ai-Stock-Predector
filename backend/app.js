1d35e2c911cf8a685554cc826336ccc27f689b94
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-04-01 21:06:09 +0530

    user model done

---
+++ backend/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"));
+
+app.listen(5000, () => console.log("Server running on port 5000"));
