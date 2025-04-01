d51bd57cd1688c6e0436f876bab65342917ff42e
samiulhoquechowdhury <samiulofficialworks@gmail.com>
2025-04-01 21:48:24 +0530

    service api

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,33 +1,33 @@
-// import axios from "axios";
+import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api";
-// const ML_URL = "http://localhost:5001";
+const BASE_URL = "http://localhost:5000/api";
+const ML_URL = "http://localhost:5001";
 
-// interface StockHistory {
-//   date: string;
-//   open: number;
-//   high: number;
-//   low: number;
-//   close: number;
-//   volume: number;
-// }
+interface StockHistory {
+  date: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+}
 
-// interface PredictionResponse {
-//   predicted_price: number;
-// }
+interface PredictionResponse {
+  predicted_price: number;
+}
 
-// export const fetchStockData = async (
-//   symbol: string
-// ): Promise<StockHistory[]> => {
-//   const { data } = await axios.get<StockHistory[]>(
-//     `${BASE_URL}/stocks/history/${symbol}`
-//   );
-//   return data;
-// };
+export const fetchStockData = async (
+  symbol: string
+): Promise<StockHistory[]> => {
+  const { data } = await axios.get<StockHistory[]>(
+    `${BASE_URL}/stocks/history/${symbol}`
+  );
+  return data;
+};
 
-// export const getStockPrediction = async (symbol: string): Promise<number> => {
-//   const { data } = await axios.get<PredictionResponse>(
-//     `${ML_URL}/predict?symbol=${symbol}`
-//   );
-//   return data.predicted_price;
-// };
+export const getStockPrediction = async (symbol: string): Promise<number> => {
+  const { data } = await axios.get<PredictionResponse>(
+    `${ML_URL}/predict?symbol=${symbol}`
+  );
+  return data.predicted_price;
+};
